59a61e82561e4b6787b331ea4a6cd60a52b45893
dcipani <33790535+Dcipani@users.noreply.github.com>
2024-09-24 04:11:36 +0100

    smooth scroll

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -12,28 +12,14 @@ const Navbar = ({ activeSection }) => {
     setMenuVisible(!menuVisible);
     setJustToggled(true);
   };
+  const scrollToSection = (sectionId, event) => {
+    event.preventDefault();  // Prevent the default anchor click behavior
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });  // Smooth scroll
+    }
+  };
 
-//   useEffect(() => {
-//     const activeLink = document.querySelector(`.navbar ul li.${activeSection}`);
-//     const dot = dotRef.current;
-  
-//     if (activeLink && dot && menuVisible) {
-  
-//       // Get the travel distance from the CSS variable
-//       const computedStyle = window.getComputedStyle(activeLink);
-//       const travelDistance = parseFloat(computedStyle.getPropertyValue('--travel-distance')) || 0;
-  
-//       // Calculate dot position and add travelDistance
-//       const dotPosition = -travelDistance;
-  
-//       // Apply the transform with updated dot position
-//       dot.style.transform = `translateX(${dotPosition}px)`;
-//     } else if (dot && !menuVisible) {
-//         dot.style.transform = `translateX(-45px) translateY(-45px)`;
-//     }
-//   }, [activeSection, menuVisible]);
-  
-  
 useEffect(() => {
     const activeLink = document.querySelector(`.navbar ul li.${activeSection}`);
     const dot = dotRef.current;
@@ -43,10 +29,9 @@ useEffect(() => {
         dot.classList.add('off-cactus');
 
 
-        // Pass a function reference to setJustToggled
         const handleAnimationEnd = () => {
-            dot.style.transform = `translate(0px, 12px)`;  // Lock the dot's final position
-            setJustToggled(false);  // This will now be called after the animation ends
+            dot.style.transform = `translate(0px, 12px)`;
+            setJustToggled(false);  
         };
 
         dot.addEventListener('animationend', handleAnimationEnd, { once: true });
@@ -67,14 +52,12 @@ useEffect(() => {
       dot.style.setProperty('--target-x', `${-travelDistance}px`);
 
       const handleAnimationEnd = () => {
-        dot.style.transform = `translateX(${-travelDistance}px) translateY(12px)`;  // Lock the dot's final position
+        dot.style.transform = `translateX(${-travelDistance}px) translateY(12px)`; 
         dot.classList.remove('to-section');
       };
   
-      // Attach listener for animation end
       dot.addEventListener('animationend', handleAnimationEnd, { once: true });
   
-      // Update CSS variable for X translation
   
     } 
   }, [activeSection, justToggled, menuVisible]);
@@ -90,19 +73,19 @@ useEffect(() => {
         <li className={`home ${activeSection === 'home' ? 'active' : ''} ${menuVisible ? 'visible' : 'hidden'}`}         
             style={{ '--travel-distance': '-75px'}} 
         >
-          <a href="#home">Home</a>
+          <a href="#home" onClick={(e) => scrollToSection('home', e)}>Home</a>
         </li>
         <li className={`about ${activeSection === 'about' ? 'active' : ''} ${menuVisible ? 'visible' : 'hidden'}`}
         style={{ '--travel-distance': '-185px'}} 
         >
-          <a href="#about">About</a>
+          <a href="#about" onClick={(e) => scrollToSection('about', e)}>About</a>
         </li>
         <li className={`projects ${activeSection === 'projects' ? 'active' : ''} ${menuVisible ? 'visible' : 'hidden'}`}
         style={{ '--travel-distance': '-300px'}} 
         >
-          <a href="#projects">Projects</a>
+          <a href="#projects" onClick={(e) => scrollToSection('projects', e)}>Projects</a>
         </li>
-        <img src={PricklyPear} ref={dotRef} className="red-dot"></img>
+        <img src={PricklyPear} alt="prickly pear" ref={dotRef} className="red-dot"></img>
       </ul>
     </div>
   );
