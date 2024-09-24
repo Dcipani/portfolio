ea181cd3e96d27ee08a2c34ca3dac19a1c4adad5
dcipani <33790535+Dcipani@users.noreply.github.com>
2024-09-25 00:04:32 +0100

    scroll through gallery

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,29 +1,54 @@
 import React, { useState } from 'react';
 import "./style.scss";
 import SectionHeader from "../SectionHeader";
-import ProjectDetail from '../ProjectDetail';
 import Project from "../Project";
 
+const Gallery = ({ projects, setSelectedProject }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
 
+  const projectsPerPage = 3; 
 
-const Gallery = ({ projects , setSelectedProject}) => {
+  const visibleProjects = projects.slice(currentIndex, currentIndex + projectsPerPage);
 
-//temp stupid comment
+  const handleNextClick = () => {
+    if (isAnimating) return; 
+
+    setIsAnimating(true); 
+
+    setTimeout(() => {
+      if (currentIndex + projectsPerPage >= projects.length) {
+        setCurrentIndex(0);
+      } else if (currentIndex + 2 * projectsPerPage >= projects.length) {
+        setCurrentIndex(currentIndex + projectsPerPage - (currentIndex + 2 * projectsPerPage - projects.length));
+      } else {
+        setCurrentIndex(currentIndex + projectsPerPage);
+      }
+
+    }, 300); 
+    setTimeout(() => {
+      setIsAnimating(false); 
+    }, 800); 
+
+  };
+
   return (
     <section className="gallery">
-      <SectionHeader title='projects'/>
-        <div className="gallery-container">
-          {projects.map((project, index) => (
-            <Project
-              key={index}
-              title={project.title}
-
-              imageSrc={`${project.imageSrc}`} // Access image in public/images
-              tags={project.tags}
-              onClick={() => setSelectedProject(index)} // Set the selected project index
-            />
-          ))}
-        </div>
+      <SectionHeader title="projects" />
+      <div className={`gallery-container ${isAnimating ? 'animating' : ''}`}>
+        {visibleProjects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            imageSrc={project.imageSrc}
+            tags={project.tags}
+            onClick={() => setSelectedProject(index)}
+          />
+        ))}
+      </div>
+      <button className="next-arrow" onClick={handleNextClick}>
+        &#9654; 
+      </button>
     </section>
   );
 };
