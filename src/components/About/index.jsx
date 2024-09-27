937a6b074e5adc6daf61322e4e6dbf94b3d7c689
dcipani <33790535+Dcipani@users.noreply.github.com>
2024-09-27 18:45:24 +0100

    fixed bug with contact links

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -12,32 +12,10 @@ const About = () => {
         {title: 'languages', points: ['english', 'italian', 'german', ' japanese', 'spanish', 'french']},
     ]
 
-    const refs = useRef([]);
     
-    useEffect(() => {
-      refs.current.forEach((ref, index) => {
-        if (ref) {
-          console.log(`Ref for card ${index} set to:`, ref);
-        }
-      });
-    }, []);
-
    
-    const [lineCoordinates, setLineCoordinates] = useState({ x1: 0, y1: 0, x2: 0, y2: 0 });
-
-  useEffect(() => {
-    if (refs.current.length >= 2) {
-      const ref1 = refs.current[0].getBoundingClientRect();
-      const ref2 = refs.current[1].getBoundingClientRect();
-
-      const x1 = ref1.x + ref1.width / 2;
-      const y1 = ref1.y + 1000;
-      const x2 = ref2.x + ref2.width / 2;
-      const y2 = ref2.y + 1000;
-
-      setLineCoordinates({ x1, y1, x2, y2 });
-    }
-  }, []);
+   
+  
   
     return (
         <section className='about-section'>
@@ -48,7 +26,6 @@ const About = () => {
                 <div key={index} className="card-wrapper">
                     <div className="card">
                         <div className="card-hole" key={index} 
-                              ref={(el) => (refs.current[index] = el)}
                         />
                         <div className="card-content">
 
@@ -64,18 +41,7 @@ const About = () => {
                 </div>
                 ))}
             </div>
-        <svg
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-        >
-            <line
-              x1={lineCoordinates.x1}
-              y1={lineCoordinates.y1}
-              x2={lineCoordinates.x2}
-              y2={lineCoordinates.y2}
-              stroke="black"
-              strokeWidth="2"
-            />
-        </svg>
+
             
         </section>
     )
